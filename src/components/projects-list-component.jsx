d69ef791c84bf7f94d17687760c6ffaffe2502fd
Anna Zdyb <89396456+anna-1980@users.noreply.github.com>
2023-11-10 10:26:10 +0100

    small fix

--- src/components/projects-list-component.jsx
+++ src/components/projects-list-component.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import { useAuth } from "../context/AuthContext";
 
 const ProjectsListComponent = ({ projects }) => {
-
   const { deleteProject } = useAuth();
-  
+
   const signedUser = localStorage.getItem("user")
     ? JSON.parse(localStorage.getItem("user"))
     : {};
 
-    const onDeleteBtn = (id, user) => {
+  const onDeleteBtn = (id, user) => {
     console.log("ID >>>", id);
     console.log("USER ID >>>", signedUser);
-    alert("Are you sure you want to delete this project?");
     deleteProject(id, signedUser);
   };
 
@@ -21,17 +19,16 @@ const ProjectsListComponent = ({ projects }) => {
       {projects.map(({ _id, url, description, title }) => {
         return (
           <div className="projects-list__item" key={_id}>
-           
             <h4>Title: {title}</h4>
 
-            <h3 style={{ color:"green" }}>{description}</h3>
+            <h3 style={{ color: "green" }}>{description}</h3>
             <button
-                        type="button"
-                        onClick={() => onDeleteBtn(_id)}
-                        className="btn-list-del"
-                      >
-                        X
-                      </button>
+              type="button"
+              onClick={() => onDeleteBtn(_id)}
+              className="btn-list-del"
+            >
+              X
+            </button>
           </div>
         );
       })}
